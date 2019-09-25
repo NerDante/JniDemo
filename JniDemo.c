e12a41ddf672a95fc5f84fe96b331c79fc0a1693
Dante <liuyongqiangx@hotmail.com>
2019-09-25 16:42:17 +0800

    Format source code

--- JniDemo.c
+++ JniDemo.c
@@ -1,85 +1,78 @@
-#include <stdio.h>
-#include <string.h>
 #include "JniDemo.h"
 #include "jni.h"
+#include <stdio.h>
+#include <string.h>
 
-JNIEXPORT void JNICALL Java_JniDemo_print(JNIEnv *env, jobject obj)
+JNIEXPORT void JNICALL Java_JniDemo_print(JNIEnv* env, jobject obj)
 {
     printf("hello JniDemo\n");
 }
 
-JNIEXPORT jstring JNICALL Java_JniDemo_getLine(JNIEnv *env, jobject obj, jstring prompt)
+JNIEXPORT jstring JNICALL Java_JniDemo_getLine(JNIEnv* env, jobject obj, jstring prompt)
 {
-   char buf[128];
-   const jbyte *str;
+    char buf[128];
+    const jbyte* str;
 
-   str = (*env)->GetStringUTFChars(env, prompt,NULL);
-   if (str == NULL) {
-       return NULL;
-   }
+    str = (*env)->GetStringUTFChars(env, prompt, NULL);
+    if (str == NULL) {
+        return NULL;
+    }
 
-   printf("%s\n", str);
-   (*env)->ReleaseStringUTFChars(env,prompt,str);
+    printf("%s\n", str);
+    (*env)->ReleaseStringUTFChars(env, prompt, str);
 
-   //assume user does not type more than 127 characters
-   scanf("%s", buf);
-   return (*env)->NewStringUTF(env, buf);
+    //assume user does not type more than 127 characters
+    scanf("%s", buf);
+    return (*env)->NewStringUTF(env, buf);
 }
 
-JNIEXPORT jint JNICALL Java_JniDemo_sumArray(JNIEnv *env, jobject obj, jintArray arr)
+JNIEXPORT jint JNICALL Java_JniDemo_sumArray(JNIEnv* env, jobject obj, jintArray arr)
 {
     jint len, i, sum = 0;
-    jint *carr;
+    jint* carr;
 
     len = (*env)->GetArrayLength(env, arr);
     printf("len = %d\n", len);
 
     carr = (*env)->GetIntArrayElements(env, arr, NULL);
-    if(carr == NULL)
-    {
+    if (carr == NULL) {
         return 0; // exception occurred
     }
 
-    for(i= 0; i < len; i++)
-    {
+    for (i = 0; i < len; i++) {
         sum += carr[i];
     }
-    
+
     (*env)->ReleaseIntArrayElements(env, arr, carr, 0);
 
     return sum;
 }
 
-JNIEXPORT jobjectArray JNICALL Java_JniDemo_initInt2DArray(JNIEnv *env, jclass cls, jint size)
+JNIEXPORT jobjectArray JNICALL Java_JniDemo_initInt2DArray(JNIEnv* env, jclass cls, jint size)
 {
     jobjectArray result;
     int i;
     jclass intArrCls;
 
     intArrCls = (*env)->FindClass(env, "[I");
-    if(intArrCls == NULL)
-    {
+    if (intArrCls == NULL) {
         return NULL;
     }
 
     result = (*env)->NewObjectArray(env, size, intArrCls, NULL);
-    if(result == NULL)
-    {
+    if (result == NULL) {
         return NULL;
     }
 
-    for (i = 0; i < size; i++)
-    {
+    for (i = 0; i < size; i++) {
         jint tmp[256]; // make sure it is large enough
         int j;
         jintArray iarr = (*env)->NewIntArray(env, size);
-        if(iarr == NULL)
-        {
+        if (iarr == NULL) {
             return NULL;
         }
 
-        for(j = 0; j < size; j++)
-        {
+        for (j = 0; j < size; j++) {
             tmp[j] = i + j;
         }
 
@@ -92,12 +85,11 @@ JNIEXPORT jobjectArray JNICALL Java_JniDemo_initInt2DArray(JNIEnv *env, jclass c
     return result;
 }
 
-
-JNIEXPORT void JNICALL Java_JniDemo_accessFiled(JNIEnv *env, jobject obj)
+JNIEXPORT void JNICALL Java_JniDemo_accessFiled(JNIEnv* env, jobject obj)
 {
     jfieldID fid_s, fid_si; // store the field ID
     jstring jstr;
-    const char *str;
+    const char* str;
     jint si;
 
     // get a reference to obj's class
@@ -106,31 +98,28 @@ JNIEXPORT void JNICALL Java_JniDemo_accessFiled(JNIEnv *env, jobject obj)
 
     // look for thefield in cls
     fid_s = (*env)->GetFieldID(env, cls, "s", "Ljava/lang/String;"); // "Ljava/lang/String" is JNI field descriptors
-    if(fid_s == NULL)
-    {
+    if (fid_s == NULL) {
         return;
     }
     fid_si = (*env)->GetStaticFieldID(env, cls, "si", "I"); // "Ljava/lang/String" is JNI field descriptors
-    if(fid_si == NULL)
-    {
+    if (fid_si == NULL) {
         return;
     }
 
     // read the field
     jstr = (*env)->GetObjectField(env, obj, fid_s);
     str = (*env)->GetStringUTFChars(env, jstr, NULL);
-    if(str == NULL) {
+    if (str == NULL) {
         return;
     }
     si = (*env)->GetStaticIntField(env, cls, fid_si);
-    
+
     printf("    jniDemo.s = \"%s\", si = %d\n", str, si);
     (*env)->ReleaseStringUTFChars(env, jstr, str);
 
     // create a new string and overwrite the instance field
     jstr = (*env)->NewStringUTF(env, "from c 123");
-    if(jstr == NULL)
-    {
+    if (jstr == NULL) {
         return;
     }
     (*env)->SetObjectField(env, obj, fid_s, jstr);
@@ -138,12 +127,11 @@ JNIEXPORT void JNICALL Java_JniDemo_accessFiled(JNIEnv *env, jobject obj)
     (*env)->SetStaticIntField(env, cls, fid_si, 200);
 }
 
-JNIEXPORT void JNICALL Java_JniDemo_nativeMethod(JNIEnv *env, jobject obj)
+JNIEXPORT void JNICALL Java_JniDemo_nativeMethod(JNIEnv* env, jobject obj)
 {
     jclass cls = (*env)->GetObjectClass(env, obj);
     jmethodID mid = (*env)->GetMethodID(env, cls, "callback", "()V");
-    if(mid == NULL)
-    {
+    if (mid == NULL) {
         return; // method not found
     }
 
@@ -151,8 +139,7 @@ JNIEXPORT void JNICALL Java_JniDemo_nativeMethod(JNIEnv *env, jobject obj)
     (*env)->CallVoidMethod(env, obj, mid);
 
     mid = (*env)->GetStaticMethodID(env, cls, "callback_static", "()V");
-    if(mid == NULL)
-    {
+    if (mid == NULL) {
         return;
     }
 
@@ -160,34 +147,31 @@ JNIEXPORT void JNICALL Java_JniDemo_nativeMethod(JNIEnv *env, jobject obj)
 }
 
 // example for return String[] in java
-JNIEXPORT jobjectArray JNICALL Java_JniDemo_getFileList(JNIEnv *env, jobject obj)
+JNIEXPORT jobjectArray JNICALL Java_JniDemo_getFileList(JNIEnv* env, jobject obj)
 {
     jobjectArray result;
     jclass cls = (*env)->GetObjectClass(env, obj);
     jclass str_cls = (*env)->FindClass(env, "java/lang/String");
-    char *list[5] = {"hallo.mp3", "lydia.mp3", "my love.mp3", "go", "中文测试"}; // fake data
+    char* list[5] = { "hallo.mp3", "lydia.mp3", "my love.mp3", "go", "中文测试" }; // fake data
 
     result = (*env)->NewObjectArray(env, 5, str_cls, NULL); // 注意：此处的jclass 是 str_cls, 而非 cls
-    if(result == NULL)
-    {
+    if (result == NULL) {
         return NULL;
     }
 
-    for (int i = 0; i < 5; i++)
-    {
+    for (int i = 0; i < 5; i++) {
         (*env)->SetObjectArrayElement(env, result, i, (*env)->NewStringUTF(env, list[i]));
     }
 
     return result;
 }
 
-JNIEXPORT jobject JNICALL Java_JniDemo_handleUserObject(JNIEnv *env, jobject demo, jobject info)
+JNIEXPORT jobject JNICALL Java_JniDemo_handleUserObject(JNIEnv* env, jobject demo, jobject info)
 {
     jclass clsIn, clsOut;
-    
+
     clsIn = (*env)->GetObjectClass(env, info);
-    if(clsIn == NULL)
-    {
+    if (clsIn == NULL) {
         printf("get object class failed\n");
         return NULL;
     }
@@ -202,7 +186,7 @@ JNIEXPORT jobject JNICALL Java_JniDemo_handleUserObject(JNIEnv *env, jobject dem
         printf("In index = %d\n", index);
 
         jstring name = (*env)->GetObjectField(env, info, strId);
-        const char *locStr = (*env)->GetStringUTFChars(env, name, &iscopy);
+        const char* locStr = (*env)->GetStringUTFChars(env, name, &iscopy);
         printf("In name = %s\n", locStr);
 
         (*env)->ReleaseStringUTFChars(env, name, locStr);
